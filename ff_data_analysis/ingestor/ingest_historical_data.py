63fc532987f8e31334b5766ab80475e566137f2b
Young Lee <lee.youngx@gmail.com>
2018-07-17 16:27:48 -0500

    Fixed data column names

--- ff_data_analysis/ingestor/ingest_historical_data.py
+++ ff_data_analysis/ingestor/ingest_historical_data.py
@@ -10,9 +10,9 @@ import pandas as pd
 # constants
 undesireable_columns = ['PPR', 'DKPt', 'FDPt', 'VBD', 'OvRank']
 
-qb_columns = ['PosRank', 'Name', 'Tm', 'Age', 'G', 'GS', 'Cmp', 'Att', 'Yds', 'TD', 'FantPt', 'Year']
-rb_columns = ['PosRank', 'Name', 'Tm', 'Age', 'G', 'GS', 'Att.1', 'Yds.1', 'Y/A', 'TD.1', 'FantPt', 'Year']
-wr_te_columns = ['PosRank', 'Name', 'Tm', 'Age', 'G', 'GS', 'Tgt', 'Rec', 'Yds.2', 'Y/R', 'TD.2', 'FantPt', 'Year']
+qb_columns = ['PosRank', 'Name', 'Team', 'Age', 'G', 'GS', 'PassCmp', 'PassAtt', 'PassYds', 'PassTD', 'FantPt', 'Year']
+rb_columns = ['PosRank', 'Name', 'Team', 'Age', 'G', 'GS', 'RushAtt', 'RushYds', 'Y/A', 'RushTD', 'FantPt', 'Year']
+wr_te_columns = ['PosRank', 'Name', 'Team', 'Age', 'G', 'GS', 'Tgt', 'Rec', 'RecYds', 'Y/R', 'RecTD', 'FantPt', 'Year']
 
 
 def get_current_year():
@@ -21,8 +21,28 @@ def get_current_year():
 
 def convert_column_types(data_frame):
     for column in data_frame:
-        if column not in ['Name', 'Tm', 'FantPos']:
+        if column not in ['Name', 'Team', 'Pos']:
             data_frame[column] = pd.to_numeric(data_frame[column])
+    return data_frame
+
+#Rk,Name,Tm,FantPos,Age,G,GS,Cmp,Att,Yds,TD,Int,Att.1,Yds.1,Y/A,TD.1,Tgt,Rec,Yds.2,Y/R,TD.2,FantPt,PosRank
+
+
+def convert_column_names(data_frame):
+    data_frame = data_frame.rename({'Rk' : 'Rank'}, axis='columns')
+    data_frame = data_frame.rename({'Unnamed: 1' : 'Name'}, axis='columns')
+    data_frame = data_frame.rename({'Tm' : 'Team'}, axis='columns')
+    data_frame = data_frame.rename({'FantPos' : 'Pos'}, axis='columns')
+    data_frame = data_frame.rename({'Cmp' : 'PassCmp'}, axis='columns')
+    data_frame = data_frame.rename({'Att' : 'PassAtt'}, axis='columns')
+    data_frame = data_frame.rename({'Yds' : 'PassYds'}, axis='columns')
+    data_frame = data_frame.rename({'TD' : 'PassTD'}, axis='columns')
+    data_frame = data_frame.rename({'Int' : 'PassInt'}, axis='columns')
+    data_frame = data_frame.rename({'Att.1' : 'RushAtt'}, axis='columns')
+    data_frame = data_frame.rename({'Yds.1' : 'RushYds'}, axis='columns')
+    data_frame = data_frame.rename({'TD.1' : 'RushTD'}, axis='columns')
+    data_frame = data_frame.rename({'Yds.2' : 'RecYds'}, axis='columns')
+    data_frame = data_frame.rename({'TD.2' : 'RecTD'}, axis='columns')
     return data_frame
 
 
@@ -32,10 +52,11 @@ def get_historical_rankings(year=(get_current_year() - 1)):
     url = 'https://www.pro-football-reference.com/years/%s/fantasy.htm#fantasy::none' % year
     data_frame = pd.read_html(url, skiprows=1, header=0)[0]
     data_frame = data_frame.drop(undesireable_columns, axis=1)
-    data_frame = data_frame.rename({'Unnamed: 1' : 'Name'}, axis='columns')
-    data_frame = data_frame[data_frame.Rk != 'Rk']
+    data_frame = convert_column_names(data_frame)
+    #data_frame = data_frame.rename({'Unnamed: 1' : 'Name'}, axis='columns')
+    data_frame = data_frame[data_frame.Rank != 'Rk']
     data_frame = convert_column_types(data_frame)
-    #gen.write_historical_rankings(data_frame, year)
+    gen.write_historical_rankings(data_frame, year)
     return data_frame
 
 
@@ -46,20 +67,20 @@ def get_past_five_historical_rankings():
         year_data_frame = get_historical_rankings(year)
         year_data_frame['Year'] = year
         data_frame = data_frame.append(year_data_frame, ignore_index=True)
-    #gen.write_historical_rankings(data_frame)
+    gen.write_historical_rankings(data_frame)
     return data_frame
 
 
 def parse_by_player_position(data_frame):
-    qb_data_frame = data_frame[data_frame.FantPos == 'QB']
-    rb_data_frame = data_frame[data_frame.FantPos == 'RB']
-    wr_data_frame = data_frame[data_frame.FantPos == 'WR']
-    te_data_frame = data_frame[data_frame.FantPos == 'TE']
+    qb_data_frame = data_frame[data_frame.Pos == 'QB']
+    rb_data_frame = data_frame[data_frame.Pos == 'RB']
+    wr_data_frame = data_frame[data_frame.Pos == 'WR']
+    te_data_frame = data_frame[data_frame.Pos == 'TE']
 
-    qb_data_frame = qb_data_frame[qb_columns].rename({'PosRank' : 'Rk'}, axis='columns')
-    rb_data_frame = rb_data_frame[rb_columns].rename({'PosRank' : 'Rk'}, axis='columns')
-    wr_data_frame = wr_data_frame[wr_te_columns].rename({'PosRank' : 'Rk'}, axis='columns')
-    te_data_frame = te_data_frame[wr_te_columns].rename({'PosRank' : 'Rk'}, axis='columns')
+    qb_data_frame = qb_data_frame[qb_columns].rename({'PosRank' : 'Rank'}, axis='columns')
+    rb_data_frame = rb_data_frame[rb_columns].rename({'PosRank' : 'Rank'}, axis='columns')
+    wr_data_frame = wr_data_frame[wr_te_columns].rename({'PosRank' : 'Rank'}, axis='columns')
+    te_data_frame = te_data_frame[wr_te_columns].rename({'PosRank' : 'Rank'}, axis='columns')
     collection = {'QB' : qb_data_frame, 'RB' : rb_data_frame, 'WR' : wr_data_frame, 'TE' : te_data_frame}
-    #gen.write_historical_position_rankings(collection)
+    gen.write_historical_position_rankings(collection)
     return collection
